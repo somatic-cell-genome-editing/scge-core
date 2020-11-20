26c268bcf5ab5604a8a3c93228efb520bb6e45fe
De Pons <jdepons@rgd-dep-mac.local>
2020-11-20 08:59:41 -0600

    updated

--- src/main/java/edu/mcw/scge/dao/implementation/ExperimentRecordDao.java
+++ src/main/java/edu/mcw/scge/dao/implementation/ExperimentRecordDao.java
@@ -72,7 +72,7 @@ public class ExperimentRecordDao extends AbstractDAO {
                 "left join delivery_system d on (d.delivery_system_id= r.delivery_system_id) " +
                 "left join application_method app on (app.application_method_id= r.application_method_id) " +
 
-                "left join guide g on (r.guide_id=g.guide_id) " +
+                "left join guide g on (x.guide_id=g.guide_id) " +
                 "left join target t on (t.target_id=r.target_id) " +
                 "left join model m on (m.model_id =r.model_id) " +
                 "where s.study_id=?";
@@ -82,11 +82,11 @@ public class ExperimentRecordDao extends AbstractDAO {
     public List<ExperimentRecord> getExperimentRecordById(int expId) throws Exception {
         String sql="select * from  experiment x  " +
                 "left join experiment_record r on (r.experiment_id=x.experiment_id) " +
-                "left join editor e on (e.editor_id= r.editor_id) " +
-                "left join delivery_system d on (d.delivery_system_id= r.delivery_system_id) " +
-                "left join guide g on (r.guide_id=g.guide_id) " +
+                "left join editor e on (e.editor_id= x.editor_id) " +
+                "left join delivery_system d on (d.delivery_system_id= x.delivery_system_id) " +
+                "left join guide g on (x.guide_id=g.guide_id) " +
                 "left join target t on (t.target_id=r.target_id) " +
-                "left join model m on (m.model_id =r.model_id) " +
+                "left join model m on (m.model_id =x.model_id) " +
                 "where x.experiment_id=?";
         ExperimentRecordQuery q=new ExperimentRecordQuery(this.getDataSource(), sql);
         return execute(q, expId);
