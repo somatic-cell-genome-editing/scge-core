3a56fca3b0d02abfa75f67ad647443e72d9fcc25
Thota <jthota@mcw.edu>
2021-01-05 12:58:59 -0600

    updated change tier objects

---
+++ src/main/java/edu/mcw/scge/datamodel/StudyAssociation.java
@@ -0,0 +1,78 @@
+package edu.mcw.scge.datamodel;
+
+import java.sql.Date;
+
+public class StudyAssociation {
+    private int studyId;
+    private int associationId;
+    private int groupId;
+    private int tier;
+    private Date createdDate;
+    private String createdBy;
+    private Date modifiedDate;
+    private String modifiedBy;
+
+    public int getStudyId() {
+        return studyId;
+    }
+
+    public void setStudyId(int studyId) {
+        this.studyId = studyId;
+    }
+
+    public int getAssociationId() {
+        return associationId;
+    }
+
+    public void setAssociationId(int associationId) {
+        this.associationId = associationId;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public void setTier(int tier) {
+        this.tier = tier;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+}
