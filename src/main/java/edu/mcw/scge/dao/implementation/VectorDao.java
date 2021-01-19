3567142de8ddf18cd5fd16656d835859a168cee9
De Pons <jdepons@rgd-dep-mac.local>
2021-01-19 12:56:43 -0600

    updated

--- src/main/java/edu/mcw/scge/dao/implementation/VectorDao.java
+++ src/main/java/edu/mcw/scge/dao/implementation/VectorDao.java
@@ -3,6 +3,7 @@ package edu.mcw.scge.dao.implementation;
 import edu.mcw.scge.dao.AbstractDAO;
 import edu.mcw.scge.dao.spring.EditorQuery;
 import edu.mcw.scge.datamodel.Editor;
+import edu.mcw.scge.datamodel.Vector;
 
 import java.util.List;
 
@@ -10,12 +11,12 @@ import java.util.List;
  * Created by jthota on 9/10/2019.
  */
 public class VectorDao extends AbstractDAO {
-    public List<Editor> getAllVectors() throws Exception {
+    public List<Vector> getAllVectors() throws Exception {
         String sql="select * from vector";
         EditorQuery q=new EditorQuery(this.getDataSource(), sql);
         return (List<Editor>)q.execute();
     }
-    public List<Editor> getVectorById(int id) throws Exception {
+    public List<Vector> getVectorById(int id) throws Exception {
         String sql="select * from vector where vector_id=?";
         EditorQuery q=new EditorQuery(this.getDataSource(), sql);
         return execute(q, id);
