d0edb83ef0f468ed15984e1702b820349bc381c0
De Pons <jdepons@rgd-dep-mac.local>
2021-02-07 16:51:15 -0600

    updated

--- src/main/java/edu/mcw/scge/dao/implementation/StudyDao.java
+++ src/main/java/edu/mcw/scge/dao/implementation/StudyDao.java
@@ -43,7 +43,7 @@ public class StudyDao extends AbstractDAO {
 
     }
     public List<Study> getStudies() throws Exception{
-        String sql="select s.*, i.institution_name, p.name as submitterName, pi.person_id as piId, pi.name as piName from study s, institution i, person p, person pi where s.lab_id=i.institution_id and s.submitter_id=p.person_id and s.pi_id=pi.person_id";
+        String sql="select s.*, i.institution_name, p.name as submitterName, pi.person_id as piId, pi.name as piName from study s, institution i, person p, person pi where s.lab_id=i.institution_id and s.submitter_id=p.person_id and s.pi_id=pi.person_id order by s.tier desc, s.submission_date desc";
         StudyQuery q=new StudyQuery(this.getDataSource(), sql);
         return execute(q);
     }
