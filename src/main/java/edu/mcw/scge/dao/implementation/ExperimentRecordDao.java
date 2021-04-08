8c917cba6eb2f326cdf07e9e9eb228fd8b3856d2
Thota <jthota@mcw.edu>
2021-04-08 17:20:31 -0500

    added tier update methods

--- src/main/java/edu/mcw/scge/dao/implementation/ExperimentRecordDao.java
+++ src/main/java/edu/mcw/scge/dao/implementation/ExperimentRecordDao.java
@@ -39,7 +39,6 @@ public class ExperimentRecordDao extends AbstractDAO {
     public List<ExperimentRecord> getExperimentRecordsByStudyId(int studyId) throws Exception {
         String sql="select s.study, r.*, e.symbol, d.ds_type, m.name as modelName, g.guide, x.type from study s join experiment x on (s.study_id=x.study_id) " +
                 "left join experiment_record r on (r.experiment_id=x.experiment_id) " +
-                " left outer join study s on x.study_id = s.study_id " +
                 "left join editor e on (e.editor_id= r.editor_id) " +
                 "left join delivery_system d on (d.ds_id= r.ds_id) " +
                 "left join application_method app on (app.application_method_id= r.application_method_id) " +
@@ -54,7 +53,6 @@ public class ExperimentRecordDao extends AbstractDAO {
     public List<ExperimentRecord> getExperimentRecordById(int expId) throws Exception {
         String sql="select s.study, r.*, e.symbol, d.ds_type, m.name as modelName, g.guide, x.type from  experiment x  " +
                 "left join experiment_record r on (r.experiment_id=x.experiment_id) " +
-                " left outer join study s on x.study_id = s.study_id " +
                 "left join editor e on (e.editor_id= r.editor_id) " +
                 "left join delivery_system d on (d.ds_id= r.ds_id) " +
                 "left join guide g on (r.guide_id=g.guide_id) " +
--- src/main/java/edu/mcw/scge/dao/spring/ExperimentRecordQuery.java
+++ src/main/java/edu/mcw/scge/dao/spring/ExperimentRecordQuery.java
@@ -19,7 +19,7 @@ public class ExperimentRecordQuery extends MappingSqlQuery<ExperimentRecord> {
         try {
             e.setExperimentName(rs.getString("experimentName"));
         }catch (Exception exception){}
-        e.setExperimentRecordName(rs.getString("name"));
+     //   e.setExperimentRecordName(rs.getString("name"));
         e.setStudyId(rs.getInt("study_id"));
         e.setGuideId(rs.getInt("guide_id"));
         e.setDeliverySystemId(rs.getInt("ds_id"));
