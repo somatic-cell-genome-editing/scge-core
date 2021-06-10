55788e2b740871144127a7523d64f91303e73706
De Pons <jdepons@rgd-dep-mac.local>
2021-06-10 13:47:38 -0500

    updated

--- src/main/java/edu/mcw/scge/dao/implementation/ExperimentRecordDao.java
+++ src/main/java/edu/mcw/scge/dao/implementation/ExperimentRecordDao.java
@@ -52,19 +52,18 @@ public class ExperimentRecordDao extends AbstractDAO {
         return execute(q, studyId);
     }
 
-    /*public List<ExperimentRecord> getExperimentRecordById(int expId) throws Exception {
-        String sql="select s.study, r.*, e.symbol, d.ds_type, m.name as modelName, g.guide, x.type from  experiment x  " +
-                "left join experiment_record r on (r.experiment_id=x.experiment_id) " +
-                "left join editor e on (e.editor_id= r.editor_id) " +
-                "left join delivery_system d on (d.ds_id= r.ds_id) " +
-                "left join guide g on (r.guide_id=g.guide_id) " +
-              //  "left join target t on (t.target_id=r.target_id) " +
-                "left join model m on (m.model_id =r.model_id) " +
-                "where x.experiment_id=?";
+    public List<ExperimentRecord> getExperimentRecordById(int recordId) throws Exception {
+        String sql="select s.study, r.*, e.symbol, d.ds_type, m.name as modelName, x.type from study s join experiment x on (s.study_id=x.study_id) \n" +
+                "inner join experiment_record r on (r.experiment_id=x.experiment_id) \n" +
+                "left outer join editor e on (e.editor_id= r.editor_id) \n" +
+                "left outer join delivery_system d on (d.ds_id= r.ds_id) \n" +
+                "left outer join application_method app on (app.application_method_id= r.application_method_id) \n" +
+                "left outer join model m on (m.model_id =r.model_id) \n" +
+                "where r.experiment_record_id=?";
         ExperimentRecordQuery q=new ExperimentRecordQuery(this.getDataSource(), sql);
-        return execute(q, expId);
+        return execute(q, recordId);
     }
-    */
+
    /* //Not used anywhere
    public List<ExperimentRecord> getExperimentRecordByExpRecId(int expRecId) throws Exception {
         String sql="select s.study, r.*, e.symbol, d.ds_type, m.name as modelName, g.guide, x.type" +
