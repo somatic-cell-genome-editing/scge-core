10ed62328fab5c78936505838448c374e1529aba
tutajm <mtutaj@mcw.edu>
2023-01-18 15:07:11 -0600

    proper handling of NULLS and blanks in method getExpRecordId()

--- src/main/java/edu/mcw/scge/dao/implementation/ExperimentRecordDao.java
+++ src/main/java/edu/mcw/scge/dao/implementation/ExperimentRecordDao.java
@@ -105,9 +105,13 @@ public class ExperimentRecordDao extends AbstractDAO {
 
     public long getExpRecordId(ExperimentRecord r) throws Exception {
         String sql = "SELECT MAX(experiment_record_id) FROM experiment_record WHERE name=? AND study_id=? AND editor_id=? AND ds_id=? "+
-                "AND model_id=? AND sex=? AND application_method_id=? AND tissue_id=? AND cell_type=?";
+                "AND model_id=? AND application_method_id=? "+
+                // handle NULLs or blanks in columns 'sex','tissue_id','cell_type'
+                "AND COALESCE(sex,'') = COALESCE(?,'') "+
+                "AND COALESCE(tissue_id,'') = COALESCE(?,'') "+
+                "AND COALESCE(cell_type,'') = COALESCE(?,'')";
         return getLongCount(sql, r.getExperimentName(), r.getStudyId(), r.getEditorId(), r.getDeliverySystemId(),
-                r.getModelId(), r.getSex(), r.getApplicationMethodId(), r.getTissueId(), r.getCellType());
+                r.getModelId(), r.getApplicationMethodId(), r.getSex(), r.getTissueId(), r.getCellType());
     }
 
     public void addTargetTissue( List<Long> experimentRecordIds) throws Exception{
