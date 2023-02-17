e9b139ed2577756427af9998fe364d310de7c013
Thota <jthota@mcw.edu>
2023-02-17 10:34:03 -0600

    added insert method

--- src/main/java/edu/mcw/scge/dao/implementation/TierDao.java
+++ src/main/java/edu/mcw/scge/dao/implementation/TierDao.java
@@ -5,6 +5,7 @@ import edu.mcw.scge.dao.spring.IntListQuery;
 import edu.mcw.scge.dao.spring.TierQuery;
 import edu.mcw.scge.datamodel.Tier;
 
+import java.util.Date;
 import java.util.List;
 
 public class TierDao extends AbstractDAO {
@@ -20,5 +21,16 @@ public class TierDao extends AbstractDAO {
                 "last_modified_by=?," +
                 "last_modified_date=? where scge_id=?" ;
         update(sql, tier.getTier(), tier.getLastModifiedBy(), tier.getLastModifiedDate(),tier.getScgeId());
+    }
+    public void insert(Tier tier) throws Exception {
+        String sql="insert into scge_tier (" +
+                "scge_id," +
+                "tier," +
+                "last_modified_date," +
+                "last_modified_by" +
+                " )" +
+                " values(?,?,?,?)";
+        update(sql,
+                tier.getScgeId(), tier.getTier(),new Date(),tier.getLastModifiedBy());
     }
 }
