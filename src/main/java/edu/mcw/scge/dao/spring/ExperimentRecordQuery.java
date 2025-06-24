cb8c85f221007ca47096594531e62ae5033e96d9
Thota <jthota@mcw.edu>
2025-06-24 15:40:40 -0500

    added sample age

--- src/main/java/edu/mcw/scge/dao/spring/ExperimentRecordQuery.java
+++ src/main/java/edu/mcw/scge/dao/spring/ExperimentRecordQuery.java
@@ -98,6 +98,9 @@ public class ExperimentRecordQuery extends MappingSqlQuery<ExperimentRecord> {
         try {
             e.setSex(rs.getString("sex"));
         }catch (Exception e1){}
+        try {
+            e.setAge(rs.getString("age"));
+        }catch (Exception e1){}
         try{
             e.setHrdonorId(rs.getLong("hrdonor_id"));
             e.setHrdonorName(rs.getString("hrdonorName"));
